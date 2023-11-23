6488ea2b9927bc511df46c356987cb64bdb403c6
Fehm1 <fehminhuseynov03@gmail.com>
2023-11-23 19:08:29 +0400

    Front-to-back

---
+++ Figaros/Figaros.Mvc/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Figaros.Mvc.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Figaros/Figaros.Mvc/Controllers/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Figaros.Mvc.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
